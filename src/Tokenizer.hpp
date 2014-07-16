d4aab9354796dfd8ba755712e604bb54e61de094
aholic <ruochen.xu@gmail.com>
2014-07-17 01:02:37 +0800

    complete tokenize()

--- src/Tokenizer.hpp
+++ src/Tokenizer.hpp
@@ -12,10 +12,10 @@ namespace Darwin {
         public:
             vector<string> split(const string& sentence, const string& delims = " ") const {
                 vector<string> words;
-                string::size_type length = sentence.length();
+                auto length = sentence.length();
                 words.reserve(length / avgWordLength);
 
-                string::size_type pos = 0, lastPos = 0;
+                decltype(length) pos = 0, lastPos = 0;
                 while (pos != length) {
                     pos = sentence.find_first_of(delims, lastPos);
                     if (pos == string::npos) pos = length;
@@ -30,10 +30,22 @@ namespace Darwin {
             
             vector<WordIdType> tokenize(const string& sentence) {
                 auto words = split(sentence);
-                for_each(words.begin(), words.end(), []);
+                vector<WordIdType> ret;
+                for (const auto word : words) {
+                    ret.push_back(_update(word));
+                }
+                return ret;
             }
 
         private:
+            WordIdType _update(const string& word) {
+                auto wordInfo = _wordMap.find(word);
+                if (wordInfo != _wordMap.end()) return wordInfo->second; 
+
+                auto wordId = _wordMap.size();
+                _wordMap[word] = wordId;
+                return wordId;
+            }
             size_t avgWordLength = 5;
             WordMapType _wordMap;
     };
